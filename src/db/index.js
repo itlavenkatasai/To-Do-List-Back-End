8bb19cd9ef6cd3de81b4c81fed8c14b7df2c5d11
itlavenkatasai <itlavenkatasai@gmail.com>
2024-07-11 16:52:09 +0530

    mongo atlas addded

--- src/db/index.js
+++ src/db/index.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 export const connectToMongoDB = () => {
     try {
-        const { MONGODB_URL } = process.env;
-        mongoose.connect(MONGODB_URL);
+        const { DATABASE } = process.env;
+        mongoose.connect(DATABASE);
         console.log('node is connected to db');
         return true;
     } catch (error) {
